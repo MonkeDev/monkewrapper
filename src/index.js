77d06d28186102be5053df054ce91819a0c995a2
BurnedBed <53361510+BurnieBed@users.noreply.github.com>
2021-01-22 16:25:10 -0600

    yes

--- src/index.js
+++ src/index.js
@@ -5,13 +5,27 @@ const err = require('./errors.json');
 /**
  * Make a request with the MonkeDev API
  */
-const monke = async (endpoint, /*options*/) => {
+const monke = async (endpoint, args) => {
     this.endpoint = endpoint;
 
     if(!e.endpoints.includes(this.endpoint)) throw err.cannotFind;
 
+        if(this.endpoint === "/npm") {
+            let query;
+            if(args.query) 
+                query = args.query;
+            else 
+                throw err.addQuery;
+
+            const res = await p({
+                url: `https://monke.vip/api/npm?name=${query}`,
+                parse: "json"
+            });
+
+            return res.body;
+        }
         const res = await p({
-            url: `https://monke.vip/api/${this.endpoint}`,
+            url: `https://monke.vip/api${this.endpoint}`,
             parse: "json"
         });
             return res.body;
